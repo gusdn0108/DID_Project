12be0b40282433e650738398c41862c1e96ff8d1
이승준 <chronica@iseungjun-ui-MacBookAir.local>
2022-08-22 16:35:20 +0900

    did login register

--- client_server/front_b/pages/mypage.jsx
+++ client_server/front_b/pages/mypage.jsx
@@ -1,24 +1,52 @@
 import {useState,useEffect} from 'react';
 import { Box, Button, Flex, Text, Input, FormControl,
     FormLabel, FormErrorMessage, FormHelperText,} from "@chakra-ui/react";
+import axios from 'axios';
+import { backend } from '../utils/ip';
 
 
 const mypage = ({userId, email, point})  => {
-    
+    const [tryDid, setTryDid] = useState(false)
+    const [ pw, setPw ] = useState(undefined) 
+
     const didRegister = () => {
-        console.log('did 연동 페이지로 이동!')
-        console.log(userId)
+        setTryDid(true)
+    }
+
+    const getPs = (e) => {
+        setPw(e.target.value)
+    }
+
+    const didLoginHandler = async () => {
+        const response = await axios.post(`${backend}/api/auth/oauthRegister`, {email, password : pw})
+        console.log(response.data)
     }
 
     return (
         <Box pt='6rem'>
-            <Text>{userId}님 안녕하세요</Text>
-            <Text> email : {email}</Text>
+            {
+                tryDid == false 
+                ?
+            <>
+                <Text>{userId}님 안녕하세요</Text>
+                <Text> email : {email}</Text>
 
-            <Text>point : {point}</Text>
-            <Button onClick={didRegister}>
-                DID login 연동하기
-            </Button>
+                <Text>point : {point}</Text>
+                <Button onClick={didRegister}>
+                    DID login 연동하기
+                </Button>
+            </>
+            :
+            <>
+                <FormControl>
+                    <Text> 비밀번호를 다시 한 번 입력해주세요. </Text>
+
+                    <FormLabel fontSize='2xl' mb='2.5'>Password</FormLabel>
+                    <Input type='password' placeholder='password을 입력해주세요' size='md' id='userPw' mb='5' onChange={getPs}/>
+                    <Button onClick={didLoginHandler} colorScheme='blue' w='32' m='0.25rem'>DID register</Button>
+                </FormControl>
+            </>
+            }
         </Box>
     )
 }
