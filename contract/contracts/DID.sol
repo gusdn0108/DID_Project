9a0b450e93becda9f76bfb8e471a9df858976351
jihyun kim <jihyunivy@gmail.com>
2022-08-22 14:36:13 +0900

    [contract] fix: user props

--- contract/contracts/DID.sol
+++ contract/contracts/DID.sol
@@ -7,44 +7,52 @@ contract DID is IDID {
     mapping(string => User) private user;
     mapping(string => bool) private registeredUser;
 
-    constructor(string memory _publicKey){
-        user[_publicKey] = User('ivy','yellow_w@gmail.com');
+    constructor(string memory _hashedId){
+        user[_hashedId] = User('yellow_w@gmail.com','1234',false,false,false,false);
     }
 
-    function registerUser (string memory _publicKey ,User memory _data) external{
-        require(!registeredUser[_publicKey]);
+    function registerUser (string memory _hashedId ,User memory _data) external{
+        require(!registeredUser[_hashedId]);
         User memory userObj;
 
-        userObj.id = _data.id;
         userObj.email = _data.email;
+        userObj.password = _data.password;
+        userObj.A = _data.A;
+        userObj.B = _data.B;
+        userObj.C = _data.C;
+        userObj.D = _data.D;
 
-        user[_publicKey] = userObj;
-        registeredUser[_publicKey] = true;
+        user[_hashedId] = userObj;
+        registeredUser[_hashedId] = true;
 
     }
 
-    function updateUser (string memory _publicKey, User memory _data) external{
-        require(registeredUser[_publicKey]);
+    function updateUser (string memory _hashedId, User memory _data) external{
+        require(registeredUser[_hashedId]);
         User memory userObj;
 
-        userObj.id = _data.id;
         userObj.email = _data.email;
+        userObj.password = _data.password;
+        userObj.A = _data.A;
+        userObj.B = _data.B;
+        userObj.C = _data.C;
+        userObj.D = _data.D;
 
-        user[_publicKey] = userObj;
+        user[_hashedId] = userObj;
     }
 
-    function deleteUser (string memory _publicKey) external{
-        require(registeredUser[_publicKey]);
+    function deleteUser (string memory _hashedId) external{
+        require(registeredUser[_hashedId]);
 
-        delete user[_publicKey];
-        registeredUser[_publicKey] = false;
+        delete user[_hashedId];
+        registeredUser[_hashedId] = false;
     }
     
-    function getUser (string memory _publicKey) view external returns(User memory){
-        return user[_publicKey];
+    function getUser (string memory _hashedId) view external returns(User memory){
+        return user[_hashedId];
     }
 
-    function isRegistered(string memory _publicKey) view public returns(bool){
-        return registeredUser[_publicKey];
+    function isRegistered(string memory _hashedId) view public returns(bool){
+        return registeredUser[_hashedId];
     }
 }
