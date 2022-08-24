04ef7b2f0f038991d13aab6e29f15d5dd8f96875
이승준 <chronica@iseungjun-ui-MacBookAir.local>
2022-08-24 19:13:51 +0900

    pwchange

--- client_server/front_b/pages/changePw.jsx
+++ client_server/front_b/pages/changePw.jsx
@@ -6,12 +6,24 @@ import { backend,frontend } from '../utils/ip';
 
 
 const mypage = ({userId, email})  => { 
+    const [password, setPassword] = useState('')
+    
+    const getPw = (e) => {
+        setPassword(e.target.value)
+    }
 
-    return (
+    const changePw = async () => {
+        const response = await axios.post(`${backend}/api/auth/updateUser`, { email, password })
+        alert(response.data.status)
+    }
+
+    return (    
         <Box pt='6rem'>
             <Box w='30%' mx='auto' my='0' fontSize={'125%'} justifyContent='center'>
                 <FormControl mt='3'>
-                    <Input type='password' placeholder='password을 입력해주세요' size='md' id='userPw' mb='3%'/>
+                    <Input type='password' placeholder='password을 입력해주세요' size='md' id='userPw' mb='3%'
+                    onChange={getPw}/>
+                    <Button onClick={changePw}>변경하기!!</Button>
                 </FormControl>    
             </Box>   
         </Box>
