562b6220c674abf6cad55704528ea1b1df1fab6d
안수환 <ansuhwan@ansuhwan-ui-MacBookAir.local>
2022-08-24 17:45:57 +0900

    optimism-goerli

---
+++ truffle/contracts/IDID.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+interface IDID {
+    
+    struct User{
+        string email;
+        string password;
+        bool A;
+        bool B;
+        bool C;
+        bool D;
+    }
+
+    function registerUser (string memory _hashedId ,User memory _data) external;
+    function updateUser (string memory _hashedId, User memory _data) external;
+    function deleteUser (string memory _hashedId) external;
+    function getUser (string memory _hashedId) view external returns(User memory);
+    function isRegistered(string memory _hashedId) view external returns(bool);
+}
