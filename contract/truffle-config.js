4489f3144786830fe11f9c64097b87ccab00b7d7
jihyun kim <jihyunivy@gmail.com>
2022-08-31 12:42:04 +0900

    [contract] modify network

--- contract/truffle-config.js
+++ contract/truffle-config.js
@@ -21,8 +21,9 @@
 // const fs = require('fs');
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
 
-// const HDWalletProvider = require("@truffle/hdwallet-provider");
-// require("dotenv").config();
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+require("dotenv").config();
+const provider = process.env.PROVIDER;
 
 module.exports = {
 	/**
@@ -40,7 +41,7 @@ module.exports = {
 			provider: () => {
 				return new HDWalletProvider(
 					YOUR_GOERLI_MNEMONIC_GOES_HERE,
-					"https://goerli.optimism.io"
+					provider
 				);
 			},
 			network_id: "420",
@@ -51,11 +52,11 @@ module.exports = {
 		// tab if you use this network and you must also set the `host`, `port` and `network_id`
 		// options below to some value.
 		//
-		development: {
-			host: "127.0.0.1", // Localhost (default: none)
-			port: 8545, // Standard Ethereum port (default: none)
-			network_id: "*", // Any network (default: none)
-		},
+		// development: {
+		// 	host: "127.0.0.1", // Localhost (default: none)
+		// 	port: 8545, // Standard Ethereum port (default: none)
+		// 	network_id: "*", // Any network (default: none)
+		// },
 		//
 		// An additional network, but with some advanced options…
 		// advanced: {
