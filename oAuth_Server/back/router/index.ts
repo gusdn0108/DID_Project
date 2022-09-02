08297c1ad81d6cdb2e4cae44d2fd648035427db3
안수환 <ansuhwan@ansuhwan-ui-MacBookAir.local>
2022-09-02 11:42:42 +0900

    email test

--- oAuth_Server/back/router/index.ts
+++ oAuth_Server/back/router/index.ts
@@ -7,8 +7,8 @@ import verifyRouter from './verify/verifyEmail';
 const route = express.Router();
 
 route.use('/app', appRouter);
-// route.use('/login',loginRouter);
-route.use('/user',userRouter);
-route.use('/verifyEmail',verifyRouter);
+route.use('/login', loginRouter);
+route.use('/user', userRouter);
+route.use('/verifyEmail', verifyRouter);
 
 export default route;
--- oAuth_Server/back/router/verify/verifyEmail.ts
+++ oAuth_Server/back/router/verify/verifyEmail.ts
@@ -1,13 +1,14 @@
-import express,{Request, Response} from 'express';
+import express, { Request, Response } from 'express';
 import nodeMailer, { SentMessageInfo } from 'nodemailer';
+import emailTemplate from './emailTemplate';
 const router = express.Router();
 
-router.post('/email', async (req: Request, res:Response) => {
+router.post('/email', async (req: Request, res: Response) => {
     const { email } = req.body;
 
     const generateRandom = (min: number, max: number) => {
         const ranNum = (Math.floor(Math.random() * (max - min + 1)) + min).toString();
-    
+
         const array = [];
         for (let i = 0; i < 6; i++) {
             array.push(ranNum[i]);
@@ -16,6 +17,7 @@ router.post('/email', async (req: Request, res:Response) => {
     };
 
     const number = generateRandom(111111, 999999);
+
     const mailPoster = nodeMailer.createTransport({
         service: 'Naver',
         host: 'smtp.naver.com',
@@ -25,131 +27,23 @@ router.post('/email', async (req: Request, res:Response) => {
             pass: process.env.EMAIL_PASSWORD,
         },
     });
+
     let mailOptions = {
         from: 'gusdn6671@naver.com',
         to: email,
         subject: '인증번호 입력해주세요 ',
-        html: `<div
-        style='
-        text-align: center;
-        width: 60%;
-        height: 50%;
-        margin: 15%;
-        padding: 20px;
-        border:2px solid #FFB6C1;
-        border-radius: 10px;
-        background-color:#FFFAFA;
-        '>
-
-        <h2 style='
-        color:pink;
-        font-weight:bold;
-        '>아래 6자리 숫자를 화면에 입력해주세요.</h2> <br/>
-        <div style='display: flex;
-            justify-content: space-between ;
-           '>
-           <div style='  width:5rem;
-           height:5rem;
-           border:2px solid #FFB6C1;
-           border-radius: 10px;
-           background-color:#FFFAFA;'>
-           <h1 style='
-           text-align:center;
-           font-weight:bold;
-           font-size:47px;
-           color:#FFB6C1;'>
-           ${number[0]}
-           </h1>
-
-        </div>
-        <div style='  width:5rem;
-           height:5rem;
-           border:2px solid #FFB6C1;
-           border-radius: 10px;
-           background-color:#FFFAFA;'>
-           <h1 style='
-           text-align:center;
-           font-weight:bold;
-           font-size:47px;
-           color:#FFB6C1;'>
-           ${number[1]}
-           </h1>
-
-        </div>
-        <div style='  width:5rem;
-           height:5rem;
-           border:2px solid #FFB6C1;
-           border-radius: 10px;
-           background-color:#FFFAFA;'>
-           <h1 style='
-           text-align:center;
-           font-weight:bold;
-           font-size:47px;
-           color:#FFB6C1;'>
-           ${number[2]}
-           </h1>
-
-        </div>
-
-        <div style='  width:5rem;
-           height:5rem;
-           border:2px solid #FFB6C1;
-           border-radius: 10px;
-           background-color:#FFFAFA;'>
-           <h1 style='
-           text-align:center;
-           font-weight:bold;
-           font-size:47px;
-           color:#FFB6C1;'>
-           ${number[3]}
-           </h1>
-
-        </div>
-
-        <div style='  width:5rem;
-           height:5rem;
-           border:2px solid #FFB6C1;
-           border-radius: 10px;
-           background-color:#FFFAFA;'>
-           <h1 style='
-           text-align:center;
-           font-weight:bold;
-           font-size:47px;
-           color:#FFB6C1;'>
-           ${number[4]}
-           </h1>
-
-        </div>
-
-        <div style='  width:5rem;
-           height:5rem;
-           border:2px solid #FFB6C1;
-           border-radius: 10px;
-           background-color:#FFFAFA;'>
-           <h1 style='
-           text-align:center;
-           font-weight:bold;
-           font-size:47px;
-           color:#FFB6C1;'>
-           ${number[5]}
-           </h1>
-
-        </div>
-
-   </div>
-
-</div> `,
+        html: emailTemplate(number),
     };
 
-    mailPoster.sendMail(mailOptions, function (error :Error|null, info: SentMessageInfo) {
+    mailPoster.sendMail(mailOptions, function (error: Error | null, info: SentMessageInfo) {
         if (error) {
             console.log(error);
         } else {
             console.log('Email sent: ' + info.response);
-            res.status(201)
+            res.status(201);
             res.json({
                 status: true,
-                number: number,
+                number,
             });
         }
     });
