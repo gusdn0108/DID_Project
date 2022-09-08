d33f80e17e8bca3334551209a7220697f1adfb90
안수환 <ansuhwan@ansuhwan-ui-MacBookAir.local>
2022-09-08 12:22:20 +0900

    usePoint test

---
+++ oAuth_Server/back/router/point/usepoint.ts
@@ -0,0 +1,10 @@
+import express, { Request, Response } from 'express';
+import { Op } from 'sequelize';
+import jwt from 'jsonwebtoken';
+import axios from 'axios';
+import TotalPoint from '../../models/user/totalPoint.model';
+
+const router = express.Router();
+
+/** 최초 결제 모듈 페이지 접속시 해당 유저의 가입된 사이트의 포인트를 전달해줌 */
+router.post('/');
