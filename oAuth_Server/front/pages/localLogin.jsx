1e4e746baea83e446a3916a47e73401453314ee3
이승준 <chronica@iseungjun-ui-MacBookAir.local>
2022-09-20 19:41:39 +0900

    oauth_front ui

--- oAuth_Server/front/pages/localLogin.jsx
+++ oAuth_Server/front/pages/localLogin.jsx
@@ -3,7 +3,8 @@ import axios from 'axios';
 import { useState } from 'react';
 import { setCookie } from 'cookies-next';
 import { backend, frontend } from '../utils/ip';
-export default function Home() {
+import Header from '../components/Header';
+export default function Home({ user }) {
   const [DIDid, setDIDid] = useState(undefined);
   const [DIDPw, setDIDPw] = useState(undefined);
   const [loading, setLoading] = useState(true);
@@ -38,59 +39,62 @@ export default function Home() {
   };
 
   return (
-    <Box bg="#160627" w="100%" h="59rem">
-      <Flex w="60%" mx="auto" pt="12rem" justifyContent={'center'}>
-        <Box w="50%" mx="3%" px="5%" py="6%">
-          <Text fontSize={'1.5rem'} mb="2%" color="#fff">
-            DID login으로 a/b/c/d 사이트를 이용해보세요!
-          </Text>
-          <Text fontSize="0.75rem" mb="0.5%" color="#fff">
-            a/b/c/d 사이트는 DID login으로 이용할 수 있습니다
-          </Text>
-          <Text fontSize="0.75rem" mb="4%" color="#fff">
-            사용 중인 DID계정으로 로그인해 보세요
-          </Text>
-          <Image mr="1%" src="https://accounts.kakao.com/assets/weblogin/techin/retina/banner_login2-7800b65948f0912306346a56a61832a98aa302c7e6cf3411eacd35db47d53a3c.png"></Image>
-        </Box>
+    <>
+      <Header user={user} />
+      <Box bg="#160627" w="100%" h="65rem">
+        <Flex w="60%" mx="auto" pt="16rem" justifyContent={'center'}>
+          <Box w="50%" mx="3%" px="5%" py="6%">
+            <Text fontSize={'1.5rem'} mb="2%" color="#fff">
+              DID login으로 a/b/c/d 사이트를 이용해보세요!
+            </Text>
+            <Text fontSize="0.75rem" mb="0.5%" color="#fff">
+              a/b/c/d 사이트는 DID login으로 이용할 수 있습니다
+            </Text>
+            <Text fontSize="0.75rem" mb="4%" color="#fff">
+              사용 중인 DID계정으로 로그인해 보세요
+            </Text>
+            <Image mr="1%" src="https://accounts.kakao.com/assets/weblogin/techin/retina/banner_login2-7800b65948f0912306346a56a61832a98aa302c7e6cf3411eacd35db47d53a3c.png"></Image>
+          </Box>
 
-        <Box w="35%" mx="3%" border={'1px'} borderColor="gray.200" px="5%" py="5%">
-          <Text fontSize={'2rem'} mb="1rem" color="#fff">
-            DID Service
-          </Text>
-          <FormControl mb="1rem">
-            <FormLabel fontSize="xl" mb="2.5" color="#fff">
-              Email
-            </FormLabel>
-            <Input type="text" placeholder="email을 입력해주세요" size="md" id="Email" mb="7%" onChange={getId} style={{ color: 'white' }} />
+          <Box w="35%" mx="3%" border={'1px'} borderColor="gray.200" px="5%" py="5%">
+            <Text fontSize={'2rem'} mb="1rem" color="#fff">
+              DID Service
+            </Text>
+            <FormControl mb="1rem">
+              <FormLabel fontSize="xl" mb="2.5" color="#fff">
+                Email
+              </FormLabel>
+              <Input type="text" placeholder="email을 입력해주세요" size="md" id="Email" mb="7%" onChange={getId} style={{ color: 'white' }} />
 
-            <FormLabel fontSize="xl" mb="2.5" color="#fff">
-              Password
-            </FormLabel>
-            <Input type="password" placeholder="password을 입력해주세요" size="md" id="userPw" mb="5%" onChange={getPw} style={{ color: 'white' }} />
-          </FormControl>
-          {loading ? (
-            <>
-              <Button onClick={didLoginHandler} color="#160627" w="100%">
-                로그인
-              </Button>{' '}
-              <Button
-                color="#160627"
-                onClick={() => {
-                  window.location.replace('/register');
-                }}
-                w="100%"
-                mt="2rem"
-              >
-                회원가입{' '}
-              </Button>
-            </>
-          ) : (
-            <Center>
-              <Spinner thickness="4px" speed="0.65s" emptyColor="gray.200" color="blue.500" size="lg" />
-            </Center>
-          )}
-        </Box>
-      </Flex>
-    </Box>
+              <FormLabel fontSize="xl" mb="2.5" color="#fff">
+                Password
+              </FormLabel>
+              <Input type="password" placeholder="password을 입력해주세요" size="md" id="userPw" mb="5%" onChange={getPw} style={{ color: 'white' }} />
+            </FormControl>
+            {loading ? (
+              <>
+                <Button onClick={didLoginHandler} color="#160627" w="100%">
+                  로그인
+                </Button>{' '}
+                <Button
+                  color="#160627"
+                  onClick={() => {
+                    window.location.replace('/register');
+                  }}
+                  w="100%"
+                  mt="2rem"
+                >
+                  회원가입{' '}
+                </Button>
+              </>
+            ) : (
+              <Center>
+                <Spinner thickness="4px" speed="0.65s" emptyColor="gray.200" color="blue.500" size="lg" />
+              </Center>
+            )}
+          </Box>
+        </Flex>
+      </Box>
+    </>
   );
 }
