679e1bb5c97e0be54838223a50c1712199cf9476
안수환 <ansuhwan@ansuhwan-ui-MacBookAir.local>
2022-09-20 12:12:07 +0900

    ip check

--- oAuth_Server/front/components/Header.jsx
+++ oAuth_Server/front/components/Header.jsx
@@ -4,59 +4,60 @@ import Image from 'next/image';
 import logo from '../image/teamlogo.png';
 import { useState, useEffect } from 'react';
 import { deleteCookie } from 'cookies-next';
+import { frontend } from '../utils/ip';
 
 const Header = ({ user }) => {
-	const [isLogin, setIsLogin] = useState(false);
+  const [isLogin, setIsLogin] = useState(false);
 
-	useEffect(() => {
-		if (user) {
-			setIsLogin(true);
-		}
-	});
+  useEffect(() => {
+    if (user) {
+      setIsLogin(true);
+    }
+  });
 
-	return (
-		<>
-			<Flex>
-				<Box>
-					<Flex justifyContent='center' alignItems='center' px='2.5rem' pb='1rem' w='full' size='sm' fontSize='2xl' position='fixed' zIndex={10} mt='6'>
-						<Box w={3000}>
-							<a href='/'>
-								<Image src={logo} width={250} height={100} />
-							</a>
-						</Box>
-						{isLogin ? (
-							<Box w={700}>
-								<Button
-									onClick={(req, res) => {
-										deleteCookie('user', { req, res, maxAge: 60 * 60 * 24 * 1000 });
-										setIsLogin(false);
-										window.location.replace('/');
-									}}
-								>
-									Logout
-								</Button>
-								<a href='http://localhost:8080/mypage'>
-									<Button ml='3'>Profile</Button>
-								</a>
-								<a href='http://localhost:8080/manageApp'>
-									<Button ml='3'>MyApp</Button>
-								</a>
-							</Box>
-						) : (
-							<Box w={450}>
-								<a href='http://localhost:8080/localLogin'>
-									<Button>Login</Button>
-								</a>
-								<a href='http://localhost:8080/register'>
-									<Button ml='3'>Sign up</Button>
-								</a>
-							</Box>
-						)}
-					</Flex>
-				</Box>
-			</Flex>
-		</>
-	);
+  return (
+    <>
+      <Flex>
+        <Box>
+          <Flex justifyContent="center" alignItems="center" px="2.5rem" pb="1rem" w="full" size="sm" fontSize="2xl" position="fixed" zIndex={10} mt="6">
+            <Box w={3000}>
+              <a href="/">
+                <Image src={logo} width={250} height={100} />
+              </a>
+            </Box>
+            {isLogin ? (
+              <Box w={700}>
+                <Button
+                  onClick={(req, res) => {
+                    deleteCookie('user', { req, res, maxAge: 60 * 60 * 24 * 1000 });
+                    setIsLogin(false);
+                    window.location.replace('/');
+                  }}
+                >
+                  Logout
+                </Button>
+                <a href={`http://${frontend}/mypage`}>
+                  <Button ml="3">Profile</Button>
+                </a>
+                <a href={`http://${frontend}/manageApp`}>
+                  <Button ml="3">MyApp</Button>
+                </a>
+              </Box>
+            ) : (
+              <Box w={450}>
+                <a href={`http://${frontend}/localLogin`}>
+                  <Button>Login</Button>
+                </a>
+                <a href={`http://${frontend}/register`}>
+                  <Button ml="3">Sign up</Button>
+                </a>
+              </Box>
+            )}
+          </Flex>
+        </Box>
+      </Flex>
+    </>
+  );
 };
 
 export default Header;
--- oAuth_Server/front/utils/ip.js
+++ oAuth_Server/front/utils/ip.js
@@ -1,3 +1,3 @@
-export const backend = "localhost:8000";
-export const frontend = "localhost:8080";
-export const cookieDomain = "localhost";
+export const backend = 'localhost:8000';
+export const frontend = '3.35.86.127:80';
+export const cookieDomain = 'localhost';
