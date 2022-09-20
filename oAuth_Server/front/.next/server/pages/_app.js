4bd2c2c141c9489b0b81e4db049399a81e0c1324
안수환 <ansuhwan@ansuhwan-ui-MacBookAir.local>
2022-09-20 11:41:32 +0900

    distribution_test

---
+++ oAuth_Server/front/.next/server/pages/_app.js
@@ -0,0 +1,263 @@
+"use strict";
+(() => {
+var exports = {};
+exports.id = 888;
+exports.ids = [888];
+exports.modules = {
+
+/***/ 9502:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": () => (/* binding */ _app)
+});
+
+// EXTERNAL MODULE: external "@chakra-ui/react"
+var react_ = __webpack_require__(8930);
+// EXTERNAL MODULE: external "cookies-next"
+var external_cookies_next_ = __webpack_require__(8982);
+// EXTERNAL MODULE: external "react"
+var external_react_ = __webpack_require__(6689);
+// EXTERNAL MODULE: ./node_modules/next/image.js
+var next_image = __webpack_require__(5675);
+var image_default = /*#__PURE__*/__webpack_require__.n(next_image);
+;// CONCATENATED MODULE: ./image/teamlogo.png
+/* harmony default export */ const teamlogo = ({"src":"/_next/static/media/teamlogo.e08bfc49.png","height":100,"width":250,"blurDataURL":"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAADCAYAAACuyE5IAAAAaElEQVR4nGN8M2eGP7OgkMXvD++n8ielff/z6eM/RlY2JgYGhs9A/Ivx3arlNzl09NS+njxWKpCQcub3588/GVlZZYCSx4H4CePbebNMmQQFDX9sXLX4f2jcdzYvHwZhBgZGoCQI/AcAPLYlyIERMOcAAAAASUVORK5CYII="});
+// EXTERNAL MODULE: external "react/jsx-runtime"
+var jsx_runtime_ = __webpack_require__(997);
+;// CONCATENATED MODULE: ./components/Header.jsx
+
+
+
+
+
+
+
+
+
+
+const Header = ({
+  user
+}) => {
+  const {
+    0: isLogin,
+    1: setIsLogin
+  } = (0,external_react_.useState)(false);
+  (0,external_react_.useEffect)(() => {
+    if (user) {
+      setIsLogin(true);
+    }
+  });
+  return /*#__PURE__*/jsx_runtime_.jsx(jsx_runtime_.Fragment, {
+    children: /*#__PURE__*/jsx_runtime_.jsx(react_.Flex, {
+      children: /*#__PURE__*/jsx_runtime_.jsx(react_.Box, {
+        children: /*#__PURE__*/(0,jsx_runtime_.jsxs)(react_.Flex, {
+          justifyContent: "center",
+          alignItems: "center",
+          px: "2.5rem",
+          pb: "1rem",
+          w: "full",
+          size: "sm",
+          fontSize: "2xl",
+          position: "fixed",
+          zIndex: 10,
+          mt: "6",
+          children: [/*#__PURE__*/jsx_runtime_.jsx(react_.Box, {
+            w: 3000,
+            children: /*#__PURE__*/jsx_runtime_.jsx("a", {
+              href: "/",
+              children: /*#__PURE__*/jsx_runtime_.jsx((image_default()), {
+                src: teamlogo,
+                width: 250,
+                height: 100
+              })
+            })
+          }), isLogin ? /*#__PURE__*/(0,jsx_runtime_.jsxs)(react_.Box, {
+            w: 700,
+            children: [/*#__PURE__*/jsx_runtime_.jsx(react_.Button, {
+              onClick: (req, res) => {
+                (0,external_cookies_next_.deleteCookie)('user', {
+                  req,
+                  res,
+                  maxAge: 60 * 60 * 24 * 1000
+                });
+                setIsLogin(false);
+                window.location.replace('/');
+              },
+              children: "Logout"
+            }), /*#__PURE__*/jsx_runtime_.jsx("a", {
+              href: "http://localhost:8080/mypage",
+              children: /*#__PURE__*/jsx_runtime_.jsx(react_.Button, {
+                ml: "3",
+                children: "Profile"
+              })
+            }), /*#__PURE__*/jsx_runtime_.jsx("a", {
+              href: "http://localhost:8080/manageApp",
+              children: /*#__PURE__*/jsx_runtime_.jsx(react_.Button, {
+                ml: "3",
+                children: "MyApp"
+              })
+            })]
+          }) : /*#__PURE__*/(0,jsx_runtime_.jsxs)(react_.Box, {
+            w: 450,
+            children: [/*#__PURE__*/jsx_runtime_.jsx("a", {
+              href: "http://localhost:8080/localLogin",
+              children: /*#__PURE__*/jsx_runtime_.jsx(react_.Button, {
+                children: "Login"
+              })
+            }), /*#__PURE__*/jsx_runtime_.jsx("a", {
+              href: "http://localhost:8080/register",
+              children: /*#__PURE__*/jsx_runtime_.jsx(react_.Button, {
+                ml: "3",
+                children: "Sign up"
+              })
+            })]
+          })]
+        })
+      })
+    })
+  });
+};
+
+/* harmony default export */ const components_Header = (Header);
+;// CONCATENATED MODULE: ./pages/_app.jsx
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); enumerableOnly && (symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; })), keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = null != arguments[i] ? arguments[i] : {}; i % 2 ? ownKeys(Object(source), !0).forEach(function (key) { _defineProperty(target, key, source[key]); }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)) : ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+
+
+
+
+
+
+
+
+function MyApp({
+  Component,
+  pageProps
+}) {
+  const {
+    0: cemail,
+    1: setEmail
+  } = (0,external_react_.useState)(undefined);
+  const {
+    0: chashId,
+    1: setHashId
+  } = (0,external_react_.useState)(undefined);
+  let userEmail = '';
+  let userHash = '';
+  let userInfo;
+  const user = (0,external_cookies_next_.getCookie)('user');
+
+  if (user) {
+    userEmail = JSON.parse(Buffer.from(user, 'base64').toString('utf-8')).email;
+    userHash = JSON.parse(Buffer.from(user, 'base64').toString('utf-8')).hashId;
+    userInfo = JSON.parse(Buffer.from(user, 'base64').toString('utf-8'));
+  }
+
+  (0,external_react_.useEffect)(() => {
+    setEmail(userEmail);
+    setHashId(userHash);
+  }, []);
+  return /*#__PURE__*/(0,jsx_runtime_.jsxs)(react_.ChakraProvider, {
+    children: [/*#__PURE__*/jsx_runtime_.jsx(components_Header, {
+      user: userInfo
+    }), /*#__PURE__*/jsx_runtime_.jsx(Component, _objectSpread(_objectSpread({}, pageProps), {}, {
+      email: cemail,
+      hashId: chashId
+    }))]
+  });
+}
+
+/* harmony default export */ const _app = (MyApp);
+
+/***/ }),
+
+/***/ 8930:
+/***/ ((module) => {
+
+module.exports = require("@chakra-ui/react");
+
+/***/ }),
+
+/***/ 8982:
+/***/ ((module) => {
+
+module.exports = require("cookies-next");
+
+/***/ }),
+
+/***/ 4957:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/head.js");
+
+/***/ }),
+
+/***/ 744:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/image-config-context.js");
+
+/***/ }),
+
+/***/ 5843:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/image-config.js");
+
+/***/ }),
+
+/***/ 8854:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/parse-path.js");
+
+/***/ }),
+
+/***/ 3297:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/remove-trailing-slash.js");
+
+/***/ }),
+
+/***/ 9232:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/utils.js");
+
+/***/ }),
+
+/***/ 6689:
+/***/ ((module) => {
+
+module.exports = require("react");
+
+/***/ }),
+
+/***/ 997:
+/***/ ((module) => {
+
+module.exports = require("react/jsx-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = __webpack_require__.X(0, [121,675], () => (__webpack_exec__(9502)));
+module.exports = __webpack_exports__;
+
+})();
---
+++ oAuth_Server/front/.next/server/pages/userAppRegister.js
@@ -0,0 +1,214 @@
+"use strict";
+(() => {
+var exports = {};
+exports.id = 932;
+exports.ids = [932];
+exports.modules = {
+
+/***/ 3553:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__),
+/* harmony export */   "getServerSideProps": () => (/* binding */ getServerSideProps)
+/* harmony export */ });
+/* harmony import */ var _chakra_ui_react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(8930);
+/* harmony import */ var _chakra_ui_react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_chakra_ui_react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(2167);
+/* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(axios__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _utils_ip_js__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(9997);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(997);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__);
+
+
+
+
+
+
+
+const userAppRegister = ({
+  getUserInfo,
+  appName,
+  restAPI,
+  redirectUri,
+  hash,
+  giveUserInfo
+}) => {
+  const attributes = getUserInfo.map((v, k) => {
+    return /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx(_chakra_ui_react__WEBPACK_IMPORTED_MODULE_0__.Box, {
+      mb: "5%",
+      fontSize: "105%",
+      children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsxs)(_chakra_ui_react__WEBPACK_IMPORTED_MODULE_0__.Flex, {
+        children: ["- ", v.att]
+      })
+    }, k);
+  });
+
+  const didRegister = async () => {
+    const codeUrl = location.href;
+    const email = codeUrl.split("?")[1].split("&")[0].split("=")[1];
+    const response = await axios__WEBPACK_IMPORTED_MODULE_1___default().post(`${_utils_ip_js__WEBPACK_IMPORTED_MODULE_3__/* .backend */ .y3}/oauth/app/userdidregister`, {
+      restAPI,
+      email,
+      point: 50000,
+      hash,
+      giveUserInfo
+    });
+
+    if (response.data.status == true) {
+      alert(response.data.msg);
+      location.href = `${_utils_ip_js__WEBPACK_IMPORTED_MODULE_3__/* .frontend */ .tQ}/login?clientId=${restAPI}&redirectUri=${redirectUri}`;
+    }
+  };
+
+  return /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.Fragment, {
+    children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsxs)(_chakra_ui_react__WEBPACK_IMPORTED_MODULE_0__.Box, {
+      border: "1px",
+      borderColor: "gray.200",
+      w: "35%",
+      mx: "auto",
+      my: "10%",
+      h: "40%",
+      px: "5%",
+      py: "6%",
+      children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsxs)(_chakra_ui_react__WEBPACK_IMPORTED_MODULE_0__.Flex, {
+        h: "10%",
+        alignItems: "center",
+        pb: "12%",
+        children: [/*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx(_chakra_ui_react__WEBPACK_IMPORTED_MODULE_0__.Img, {
+          src: "https://k.kakaocdn.net/14/dn/btqmdPkHR5M/DrrBuObYWlfrBaNkokh3J0/o.jpg",
+          w: "15%"
+        }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsxs)(_chakra_ui_react__WEBPACK_IMPORTED_MODULE_0__.Box, {
+          w: "20%",
+          mx: "5%",
+          textAlign: "center",
+          children: [/*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx(_chakra_ui_react__WEBPACK_IMPORTED_MODULE_0__.Text, {
+            fontSize: "90%",
+            pr: "15%",
+            children: appName
+          }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx(_chakra_ui_react__WEBPACK_IMPORTED_MODULE_0__.Text, {
+            fontSize: "75%",
+            color: "gray.500",
+            textAlign: "left",
+            px: "7%",
+            children: "sila"
+          })]
+        }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx(_chakra_ui_react__WEBPACK_IMPORTED_MODULE_0__.Box, {
+          w: "80%",
+          children: /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx(_chakra_ui_react__WEBPACK_IMPORTED_MODULE_0__.Text, {
+            textAlign: "left",
+            fontSize: "4%",
+            mt: "2%",
+            children: "\uC11C\uBE44\uC2A4 \uC81C\uACF5\uC744 \uC704\uD574, \uB2E4\uC74C \uC0AC\uC6A9\uC790 \uC815\uBCF4 \uC81C\uACF5\uC5D0 \uB3D9\uC758\uD574\uC8FC\uC138\uC694!"
+          })
+        })]
+      }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx(_chakra_ui_react__WEBPACK_IMPORTED_MODULE_0__.Divider, {
+        orientation: "horizontal",
+        my: "4%"
+      }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx(_chakra_ui_react__WEBPACK_IMPORTED_MODULE_0__.Flex, {
+        h: "20%",
+        fontWeight: "800",
+        children: /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx(_chakra_ui_react__WEBPACK_IMPORTED_MODULE_0__.Checkbox, {
+          children: "\uC57D\uAD00 \uB3D9\uC758\uD558\uAE30"
+        })
+      }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx(_chakra_ui_react__WEBPACK_IMPORTED_MODULE_0__.Divider, {
+        orientation: "horizontal",
+        my: "4%"
+      }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsxs)(_chakra_ui_react__WEBPACK_IMPORTED_MODULE_0__.Box, {
+        py: "2%",
+        children: [/*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx(_chakra_ui_react__WEBPACK_IMPORTED_MODULE_0__.Flex, {
+          justifyContent: "space-around",
+          py: "2%",
+          children: /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx(_chakra_ui_react__WEBPACK_IMPORTED_MODULE_0__.Text, {
+            fontSize: "80%",
+            children: "\uC57D\uAD00 \uB3D9\uC758\uB294 \uB2E4\uC74C \uC815\uBCF4\uB4E4\uC758 \uC81C\uACF5\uACFC \uC81C\uACF5 \uBAA9\uC801\uC5D0 \uB300\uD55C \uB3D9\uC758\uB97C \uD3EC\uD568\uD558\uACE0 \uC788\uC2B5\uB2C8\uB2E4."
+          })
+        }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx(_chakra_ui_react__WEBPACK_IMPORTED_MODULE_0__.Flex, {
+          py: "2%",
+          children: /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx(_chakra_ui_react__WEBPACK_IMPORTED_MODULE_0__.Box, {
+            children: attributes
+          })
+        }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx(_chakra_ui_react__WEBPACK_IMPORTED_MODULE_0__.Flex, {
+          justifyContent: "center",
+          children: /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx(_chakra_ui_react__WEBPACK_IMPORTED_MODULE_0__.Button, {
+            onClick: didRegister,
+            mt: "5%",
+            children: "\uB3D9\uC758\uD558\uACE0 \uACC4\uC18D\uD558\uAE30"
+          })
+        })]
+      })]
+    })
+  });
+};
+
+const getServerSideProps = async ctx => {
+  //oauthDB에 요청을 보내서 요청 유저 정보 데이터를 가져온다.
+  const restAPI = ctx.query.restAPI;
+  const email = ctx.query.email;
+  const redirectUri = ctx.query.redirectUri;
+  const hash = ctx.query.hash;
+  const giveUserInfo = ctx.query.giveUserInfo;
+  const response = await axios__WEBPACK_IMPORTED_MODULE_1___default().get(`${_utils_ip_js__WEBPACK_IMPORTED_MODULE_3__/* .backend */ .y3}/oauth/app/giveUserInfo?restAPI=${restAPI}`);
+  return {
+    props: {
+      getUserInfo: response.data.infos?.filter(Boolean),
+      appName: response.data.appName,
+      restAPI,
+      email,
+      redirectUri,
+      hash,
+      giveUserInfo
+    }
+  };
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (userAppRegister);
+
+/***/ }),
+
+/***/ 9997:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "tQ": () => (/* binding */ frontend),
+/* harmony export */   "y3": () => (/* binding */ backend)
+/* harmony export */ });
+/* unused harmony export cookieDomain */
+const backend = "localhost:8000";
+const frontend = "localhost:8080";
+const cookieDomain = "localhost";
+
+/***/ }),
+
+/***/ 8930:
+/***/ ((module) => {
+
+module.exports = require("@chakra-ui/react");
+
+/***/ }),
+
+/***/ 2167:
+/***/ ((module) => {
+
+module.exports = require("axios");
+
+/***/ }),
+
+/***/ 997:
+/***/ ((module) => {
+
+module.exports = require("react/jsx-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__(3553));
+module.exports = __webpack_exports__;
+
+})();
