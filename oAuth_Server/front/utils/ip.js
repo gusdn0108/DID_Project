32cbdef8058afde95c55a2ccca6cafe008e6d1bb
안수환 <ansuhwan@ansuhwan-ui-MacBookAir.local>
2022-09-21 16:28:58 +0900

    ip fix

--- oAuth_Server/front/utils/ip.js
+++ oAuth_Server/front/utils/ip.js
@@ -1,3 +1,3 @@
-export const backend = 'http://3.39.202.148';
+export const backend = 'http://13.124.225.13';
 export const frontend = 'http://3.35.86.127:80';
 export const cookieDomain = 'localhost';
